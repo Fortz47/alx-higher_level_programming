2dfdee91dbc54568f9c379d6e61fe745cc89f1c4
Atuokwu Fortune <atuokwu.fortune@gmail.com>
2023-01-04 06:55:13 -0800

    solved remaining task

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for i in range(10):
+    for k in range(10):
+        if i == 8 and k == 9:
+            print("{}{}".format(i, k), end="\n")
+        if i != k and k > i:
+            print("{}{}".format(i, k), end=", ")
---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def uppercase(str):
+    string = []
+    for i in str:
+        if ord(i) >= 97 and ord(i) <=122:
+            string.append(chr(ord(i) - 32))
+        else:
+            string.append(i)
+    String = ''.join(string)
+    print(String)
