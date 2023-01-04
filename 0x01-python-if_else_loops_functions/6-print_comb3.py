14458c00c04e18f331cbbb412722c34b4f32a5cc
Fortz47 <65767694+Fortz47@users.noreply.github.com>
2023-01-04 16:11:21 +0100

    Update 6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,5 +3,5 @@ for i in range(10):
     for k in range(10):
         if i == 8 and k == 9:
             print("{}{}".format(i, k), end="\n")
-        if i != k and k > i and i != 8 and k != 9:
+        if i != k and k > i and (i * k) != 72:
             print("{}{}".format(i, k), end=", ")
