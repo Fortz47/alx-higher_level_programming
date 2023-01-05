0e0d90f1fa61140591b3b306b9578aba67808d9b
Fortz47 <65767694+Fortz47@users.noreply.github.com>
2023-01-05 17:06:34 +0100

    Update 1-calculation.py

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -2,10 +2,10 @@
 
 if __name__ = "__main__":
   """Prints the sum, difference, multiplication and division of 10 and 5."""
-import calculator_1
-a = 10
-b = 5
-print("{} + {} = {}".format(a, b, calculator_1.add(a, b)))
-print("{} - {} = {}".format(a, b, calculator_1.sub(a, b)))
-print("{} * {} = {}".format(a, b, calculator_1.mul(a, b)))
-print("{} / {} = {}".format(a, b, calculator_1.div(a, b)))
+  import calculator_1
+  a = 10
+  b = 5
+  print("{} + {} = {}".format(a, b, calculator_1.add(a, b)))
+  print("{} - {} = {}".format(a, b, calculator_1.sub(a, b)))
+  print("{} * {} = {}".format(a, b, calculator_1.mul(a, b)))
+  print("{} / {} = {}".format(a, b, calculator_1.div(a, b)))
