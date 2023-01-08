f6a93699d2ddf34c11ab8404ec7fe81acc002161
Fortz47 <65767694+Fortz47@users.noreply.github.com>
2023-01-08 03:33:27 +0100

    Update 5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -5,6 +5,6 @@ def no_c(my_string):
     string2list = list(my_string)
     for i in string2list:
         if i == 'c' or i == 'C':
-            string2list.remove(i)
+            list.remove(string2list(i))
     list2string = ''.join(string2list)
     return list2string
