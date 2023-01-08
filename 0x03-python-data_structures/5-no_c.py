9d495e2356ad9e1db4368f164980651be7297d5a
Fortz47 <65767694+Fortz47@users.noreply.github.com>
2023-01-08 03:37:39 +0100

    Update 5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,6 +2,7 @@
 
 def no_c(my_string):
     """ removes all characters c and C from a string."""
+    
     string2list = list(my_string)
     for i in string2list:
         if i == 'c' or i == 'C':
