9c35e824b9fe2a7b172294c057eb655df2af0f26
Fortz47 <65767694+Fortz47@users.noreply.github.com>
2023-01-08 03:09:12 +0100

    Update 5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -7,8 +7,8 @@ def no_c(my_string):
     j = 0
     for i in string2list:
         if i == 'c' or i == 'C'
-        del(string2list[j])
-        j += 1
+            del(string2list[j])
+            j += 1
 
     list2String = ''.join(string2list)
     return list2String
