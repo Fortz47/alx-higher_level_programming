7d4b6958638392197ce97daffd57e22116333980
Fortz47 <65767694+Fortz47@users.noreply.github.com>
2023-01-12 04:55:25 +0100

    Update 7-update_dictionary.py

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -2,10 +2,5 @@
 
 def update_dictionary(a_dictionary, key, value):
     """ replaces or adds key/value in a dictionary."""
-
-    for k, v in a_dictionary.items():
-        if k not in a_dictionary:
-            a_dictionary[key] = value
-        else:
-            a_dictionary[k] = value
+    a_dictionary[key] = value
     return a_dictionary
