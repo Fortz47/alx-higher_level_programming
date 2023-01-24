fac90e5a7d9148e3a79c747f87eb5797a155b98d
Fortz47 <65767694+Fortz47@users.noreply.github.com>
2023-01-24 20:28:13 +0100

    Update 4-list_division.py

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -5,7 +5,7 @@ def list_division(my_list_1, my_list_2, list_length):
     function that divides element by element 2 lists.
     """
 
-     new_list = []
+    new_list = []
     for i in range(0, list_length):
         try:
             div = my_list_1[i] / my_list_2[i]
