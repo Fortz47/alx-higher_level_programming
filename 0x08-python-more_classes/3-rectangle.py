bb6dcd0fb94951f410ece38bcb510a1877a8d7e0
Atuokwu Fortune <atuokwu.fortune@gmail.com>
2023-02-02 07:34:43 -0800

    solved task 2

---
+++ 0x08-python-more_classes/3-rectangle.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""defines a rectangle class"""
+
+
+class Rectangle:
+    """defines a rectangle class"""
+    def __init__(self, width=0, height=0):
+        """initializing rectangle"""
+        self.width = width
+        self.height = height
+
+
