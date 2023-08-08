0944625aafc8c48d77c3185d259701f02a35c71b
Fortz47 <65767694+Fortz47@users.noreply.github.com>
2023-08-08 01:37:54 +0100

    Update 10-check_cycle.c

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -15,11 +15,15 @@ int check_cycle(listint_t *list)
 		return (0);
 	while (ptr->next)
 	{
-		ptrFast = ptrFast->next;
-		ptrFast = ptrFast->next;
+		if (ptrFast->next)
+			ptrFast = ptrFast->next;
+		if (ptrFast->next)
+			ptrFast = ptrFast->next;
 		if (ptr == ptrFast)
 			return (1);
 		ptr = ptr->next;
+		if (ptr == ptrFast)
+			return (1);
 	}
 	return (0);
 }
