be24693ec9cbd3c3d1c0f16b959cf4ce05d21d8d
Fortz47 <65767694+Fortz47@users.noreply.github.com>
2023-08-14 18:39:48 +0100

    Update 5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def no_c(my_string):
-    if string:
+    if my_string:
         str2list = list(my_string)
         for ch in str2list:
             if ch == 'c' or ch == 'C':
