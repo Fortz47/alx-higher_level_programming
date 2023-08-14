aea52ee4e474be56c452c56b2419a6df29c2ecef
Atuokwu Fortune <atuokwu.fortune@gmail.com>
2023-08-14 18:26:34 +0000

    task 13

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,39 @@
+#include "lists.h"
+
+/**
+ * is_palindrome - checks if a singly linked list is a palindrome.
+ * @head: pointer to pointer of first node
+ *
+ * Return: 0 if it is not a palindrome, 1 if it is a palindrome
+ */
+int is_palindrome(listint_t **head)
+{
+	listint_t *ptr = *head, *tmp;
+	int count = 0, i, len;
+
+	if (!*head)
+		return (1);
+	while(ptr)
+	{
+		count++;
+		ptr = ptr->next;
+	}
+	ptr = *head;
+	if (count % 2 == 0)
+	{
+		len = count / 2;
+		while(len)
+		{
+			tmp = *head;
+			for(i = 1; i < count; i++)
+				tmp = tmp->next;
+			if (tmp->n != ptr->n)
+				return (0);
+			ptr = ptr->next;
+			count--;
+			len--;
+		}
+		return (1);
+	}
+	return (0);
+}
