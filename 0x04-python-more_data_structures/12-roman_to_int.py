cfe829d67a8c47c0af4910886240557057007167
Atuokwu Fortune <atuokwu.fortune@gmail.com>
2023-08-18 23:43:15 +0000

    fixed pycodestyle

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 
 def roman_to_int(roman_string):
-    # create a dictionary conv to hold roman numeral key, value pairs like I=1, V=5
+    if (type(roman_string) is not str) or (roman_string is None):
+        return 0
+    # create a dictionary conv to hold roman numerals key, value pairs e.g I=1
     conv = dict(I=1, V=5, X=10, L=50, C=100, D=500, M=1000)
     # initialize a variable sum_ and tmp to zero
     sum_ = 0
@@ -17,7 +19,7 @@ def roman_to_int(roman_string):
                 tmp_v = v
                 sum_ += v
                 break
-            # if value of the next roman string index is less than the conv key value
+            # if value of next roman index is less than the conv key value
             elif k == i and v < tmp_v:
                 # subtract value of roman string from variable sum_
                 sum_ -= v
