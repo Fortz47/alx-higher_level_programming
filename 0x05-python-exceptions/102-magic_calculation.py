d29840509dfe25702d53b05686ed53c9e14a7802
Fortz47 <65767694+Fortz47@users.noreply.github.com>
2023-08-28 22:14:38 +0100

    Update 102-magic_calculation.py

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
-class TooFarException(Exception):
-    pass
+
 def magic_calculation(a, b):
     result = 0
     for i in range(1, 3):
         try:
             if i > a:
-                raise TooFarException('Too far')
+                raise Exception('Too far')
             result = result + (a ** b / i)
-        except TooFarException:
+        except ValuError:
             result = result + (b + a)
     return result
