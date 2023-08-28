97af8391091f43ea9fe76dbd5f3bb0eeefa8ac0e
Atuokwu Fortune <atuokwu.fortune@gmail.com>
2023-08-28 21:04:48 +0000

    adv task 8 and 9

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+from sys import stderr
+
+
+def safe_function(fct, *args):
+    try:
+        result = fct(args[0], args[1])
+    except (ZeroDivisionError, IndexError) as err:
+        stderr.write(f"Exception: {err}\n")
+        return None
+    else:
+        return result
