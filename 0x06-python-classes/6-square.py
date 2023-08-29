500e6d82e20e6768de058a495768c62771fd85ea
Atuokwu Fortune <atuokwu.fortune@gmail.com>
2023-08-29 23:05:54 +0000

    updated task 6

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -13,13 +13,9 @@ class Square:
             raise ValueError('size must be >= 0')
         else:
             self.__size = size
-        if not isinstance(position, tuple) or len(position) < 2:
+        if (not isinstance(position, tuple) or len(position) != 2):
             raise TypeError('position must be a tuple of 2 positive integers')
-        elif not isinstance(position[0], int):
-            raise TypeError('position must be a tuple of 2 positive integers')
-        elif not isinstance(position[1], int):
-            raise TypeError('position must be a tuple of 2 positive integers')
-        elif position[0] < 0 or position[1] < 0:
+        elif any(not isinstance(x, int) or (x < 0) for x in position):
             raise TypeError('position must be a tuple of 2 positive integers')
         else:
             self.__position = position
@@ -62,13 +58,9 @@ class Square:
     @position.setter
     def position(self, value):
         """sets the value of position"""
-        if not isinstance(value, tuple) or len(value) < 2:
+        if (not isinstance(value, tuple) or len(value) != 2):
             raise TypeError('position must be a tuple of 2 positive integers')
-        elif not isinstance(value[0], int):
-            raise TypeError('position must be a tuple of 2 positive integers')
-        elif not isinstance(value[1], int):
-            raise TypeError('position must be a tuple of 2 positive integers')
-        elif value[0] < 0 or value[1] < 0:
+        elif any(not isinstance(x, int) or (x < 0) for x in value):
             raise TypeError('position must be a tuple of 2 positive integers')
         else:
             self.__position = value
