5c8b9a08cb3178afb35e0bca0f22f769248d0910
Atuokwu Fortune <atuokwu.fortune@gmail.com>
2023-09-05 08:21:39 +0000

    created all files and solved task 0-1

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""divides all element of a matrix"""
+
+
+def matrix_divided(matrix, div):
+    """divides all element of a matrix"""
+    if isinstance(matrix, list) and len(matrix) != 0:
+        if not all(isinstance(row, list) for row in matrix):
+            raise TypeError('matrix must be a matrix (list of lists) of integers/floats')
+        for row in matrix:
+            if any(not isinstance(x, (int, float)) for x in row):
+                raise TypeError('matrix must be a matrix (list of lists) of integers/floats')
+            if any(type(x) is bool for x in row):
+                raise TypeError('matrix must be a matrix (list of lists) of integers/floats')
+        if not isinstance(div, (int, float)) or isinstance(div, bool):
+            raise TypeError('div must be a number')
+        if div == 0:
+            raise ZeroDivisionError('division by zero')
+    else:
+        raise TypeError('matrix must be a matrix (list of lists) of integers/floats')
+    if len(matrix) == 1 and len(matrix[0]) == 0:
+        return [[]]
+    if len(matrix) < 2:
+        raise TypeError('Each row of the matrix must have the same size')
+    x = len(matrix[0])
+    if any(x != len(row) for row in matrix):
+        raise TypeError('Each row of the matrix must have the same size')
+
+    div = float(div)
+    result = []
+    for row in matrix:
+        result.append(list(round(x / div, 2) for x in row))
+    return result
+
