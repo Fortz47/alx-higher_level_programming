80dcb73384b3c87cd755066667cb896b5c234daa
Atuokwu Fortune <atuokwu.fortune@gmail.com>
2023-10-18 12:40:28 +0000

    task 18

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,2 +1,6 @@
 -- displays the average temperature (Fahrenheit) by city ordered by temperature
 source temperatures.sql;
+SELECT city, AVG(value) AS 'avg_temp'
+FROM `temperatures`
+GROUP BY city
+ORDER BY `avg_temp` DESC;
