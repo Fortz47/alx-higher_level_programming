a20d158d2d4079e4f4cc5fe733e609cacd27d93e
Atuokwu Fortune <atuokwu.fortune@gmail.com>
2023-10-18 04:51:32 +0000

    task 8

---
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -0,0 +1,7 @@
+-- displays the number of records with id = 89 in
+-- the table first_table of the database hbtn_0c_0 in your MySQL server
+
+-- use database hbtn_0c_0
+USE hbtn_0c_0;
+-- displays the number or id=89 record
+SELECT COUNT(id) FROM first_table;
