5d7d5672b9a4448bdc78cb4e12b2c33123eda640
Fortz47 <65767694+Fortz47@users.noreply.github.com>
2023-10-18 08:25:13 +0100

    Update 8-count_89.sql

--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -2,4 +2,5 @@
 -- the table first_table of the database hbtn_0c_0 in your MySQL server
 
 -- displays the number or id=89 record
-SELECT COUNT(id) FROM first_table;
+SELECT COUNT(id) FROM first_table
+WHERE id = 89;
