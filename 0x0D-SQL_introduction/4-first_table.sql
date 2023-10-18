065978ce467fcf38f89514084908635c7a85d9a7
Fortz47 <65767694+Fortz47@users.noreply.github.com>
2023-10-18 08:19:37 +0100

    Update 4-first_table.sql

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,8 +1,6 @@
 -- creates a table called first_table in the current database
 -- in your MySQL server
 
--- use database hbtn_0c_0
-USE hbtn_0c_0;
 -- create table if it doesn't exist and add fields
 CREATE TABLE IF NOT EXISTS first_table (
 	id INT,
