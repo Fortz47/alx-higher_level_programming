ae56dc120e2ae9af5daeab941370a2ab6ddbd7f5
Fortz47 <65767694+Fortz47@users.noreply.github.com>
2023-10-18 12:46:01 +0100

    Update 100-move_to_utf8.sql again

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -11,4 +11,4 @@ DEFAULT CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- Alter field
 ALTER TABLE hbtn_0c_0.first_table
-MODIFY name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+DEFAULT name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
