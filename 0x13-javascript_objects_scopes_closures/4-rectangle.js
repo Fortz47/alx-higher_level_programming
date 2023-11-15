731212c246f74e4a8d6772cbd03f63aa4b2c107e
Atuokwu Fortune <atuokwu.fortune@gmail.com>
2023-11-15 17:30:26 +0100

    task 2-10

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,1 +1,36 @@
 #!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print (ch = 'X') {
+    if (this.height && this.width) {
+      for (let i = 0; i < this.height; i++) {
+        let row = '';
+        for (let j = 0; j < this.width; j++) {
+          row += ch;
+        }
+        console.log(row);
+      }
+    }
+  }
+
+  rotate () {
+    if (this.height && this.width) {
+      const width = this.width;
+      this.width = this.height;
+      this.height = width;
+    }
+  }
+
+  double () {
+    this.width = this.width * 2;
+    this.height = this.height * 2;
+  }
+}
+
+module.exports = Rectangle;
--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+
+const square = require('./5-square');
+
+class Square extends square {
+  charPrint (c) {
+    this.print(c);
+  }
+}
+
+module.exports = Square;
