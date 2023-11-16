e3932c0ce22e5c61d81ac6184c8fde51633956f0
Atuokwu Fortune <atuokwu.fortune@gmail.com>
2023-11-16 19:36:06 +0000

    task 1-3

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,1 +1,28 @@
 #!/usr/bin/python3
+"""script that lists all states from the database hbtn_0e_0_usa"""
+
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
+
+    HOST = 'localhost'
+    USR = sys.argv[1]
+    PAS = sys.argv[2]
+    DB = sys.argv[3]
+
+    try:
+        db = MySQLdb.connect(host=HOST, port=3306, user=USR, passwd=PAS, db=DB)
+        cur = db.cursor()
+        cur.execute("SELECT * FROM states WHERE name LIKE 'N%'")
+        rows = cur.fetchall()
+        for row in rows:
+            print(f'{row}')
+    except MySQLdb.Error as e:
+        print(e)
+
+    finally:
+        if cur:
+            cur.close()
+        if db:
+            db.close()
