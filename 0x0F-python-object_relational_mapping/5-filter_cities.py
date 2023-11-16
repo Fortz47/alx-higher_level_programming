8752cbe92dd33bf91a837a0c6506db5ff9a9d4e5
Fortune Atuokwu <65767694+Fortz47@users.noreply.github.com>
2023-11-16 23:59:17 +0100

    Update 5-filter_cities.py

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""script that lists all states from the database hbtn_0e_0_usa"""
+"""takes in the name of a state as an argument and lists all
+cities of that state, using the database hbtn_0e_4_usa"""
 
 import MySQLdb
 import sys
