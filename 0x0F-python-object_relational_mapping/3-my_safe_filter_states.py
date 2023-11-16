5493fa42de9d589824ebbd5288212dd1b6b4847f
Fortune Atuokwu <65767694+Fortz47@users.noreply.github.com>
2023-11-17 00:01:31 +0100

    Update 3-my_safe_filter_states.py

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""script that lists all states from the database hbtn_0e_0_usa"""
+"""script that takes in an argument and displays all values in
+the states table of hbtn_0e_0_usa where name matches the argument.
+and handles SQL Injection"""
 
 import MySQLdb
 import sys
