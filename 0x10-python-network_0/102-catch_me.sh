d251129fb66d6cf8762ca28b92a0c44657049cf5
Fortune Atuokwu <65767694+Fortz47@users.noreply.github.com>
2023-12-04 11:34:23 +0100

    Update 102-catch_me.sh

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!bin/bash
 # a Bash script that makes a request to 0.0.0.0:5000/catch_me that causes the server to respond with a message containing You got me!, in the body of the response.
-curl -sLX PUT -d "You got me!" -o /dev/null -w 'You got me!\n' 0.0.0.0:5000/catch_me
+curl -sLX PUT -H "Origin: School" -d "user_id=98" 0.0.0.0:5000/catch_me
