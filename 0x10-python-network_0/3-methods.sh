bed98cd2b6cd37079e0de3a09b1fd20c576974eb
Atuokwu Fortune <atuokwu.fortune@gmail.com>
2023-12-04 10:03:14 +0000

    task 4-6 and advanced

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script that takes in a URL and displays all HTTP methods the server will accept.
-curl -sIX OPTIONS "$1" | grep "Allow:" | sed 's/Allow: \(.*\)/\1/p'
+curl -sIX OPTIONS "$1" | grep "Allow" | sed -n 's/Allow: \(.*\)/\1/p'
--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+"""finds the peak in a list of unsorted integers"""
+
+def find_peak(list_of_integers):
+    """finds the peak in a list of unsorted integers"""
+    if len(list_of_integers) == 0:
+        return None
+    list_of_integers.sort()
+    return list_of_integers[-1]
