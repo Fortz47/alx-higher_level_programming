4c6ad7b598181c61bedc97b3b8b1a943048cf3a9
Atuokwu Fortune <atuokwu.fortune@gmail.com>
2024-01-04 01:48:06 +0000

    task 101-starwars_characters.js

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,25 +1,46 @@
 #!/usr/bin/node
 
-const id = process.argv[2];
-
 const request = require('request');
 
-const url = 'https://swapi-api.alx-tools.com/api/films/' + id;
-
-request.get(url, (err, res, body) => {
-  if (err) {
-    console.error(err);
-    return;
-  }
-  const bodyObj = JSON.parse(body);
-  bodyObj.characters.forEach((item) => {
-    request(item, (err, res, body) => {
-      if (err) {
-        console.error(err);
-        return;
+const getCharacterDetails = async (characterUrl) => {
+  return new Promise((resolve, reject) => {
+    request(characterUrl, (error, response, body) => {
+      if (!error && response.statusCode === 200) {
+        const character = JSON.parse(body);
+        resolve(character.name);
+      } else {
+        reject(error);
       }
-      const Obj = JSON.parse(body);
-      console.log(Obj.name);
     });
   });
-});
+};
+
+const getMovieDetails = async (movieId) => {
+  return new Promise((resolve, reject) => {
+    const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+    request(apiUrl, (error, response, body) => {
+      if (!error && response.statusCode === 200) {
+        const film = JSON.parse(body);
+        resolve(film.characters);
+      } else {
+        reject(error);
+      }
+    });
+  });
+};
+
+const printCharacterNames = async (movieId) => {
+  try {
+    const characterUrls = await getMovieDetails(movieId);
+
+    for (const characterUrl of characterUrls) {
+      const characterName = await getCharacterDetails(characterUrl);
+      console.log(characterName);
+    }
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const movieId = process.argv[2];
+printCharacterNames(movieId);
