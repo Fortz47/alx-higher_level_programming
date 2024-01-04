c885b011e9469eb4bf935714c41a0e33b479aa44
Atuokwu Fortune <atuokwu.fortune@gmail.com>
2024-01-04 01:20:30 +0000

    task 100-starwars_characters.js

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,1 +1,25 @@
 #!/usr/bin/node
+
+const id = process.argv[2];
+
+const request = require('request');
+
+const url = 'https://swapi-api.alx-tools.com/api/films/' + id;
+
+request.get(url, (err, res, body) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  const bodyObj = JSON.parse(body);
+  bodyObj.characters.forEach((item) => {
+    request(item, (err, res, body) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      const Obj = JSON.parse(body);
+      console.log(Obj.name);
+    });
+  });
+});
