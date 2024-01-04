45166fc4e260b93168257a71285c3943df8e68ed
Atuokwu Fortune <atuokwu.fortune@gmail.com>
2024-01-04 00:23:27 +0000

    task 5

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,1 +1,12 @@
 #!/usr/bin/node
+
+const url = process.argv[2];
+const filePath = process.argv[3];
+const fs = require('fs');
+const request = require('request');
+
+request(url, (err) => {
+  if (err) {
+    console.log(err);
+  }
+}).pipe(fs.createWriteStream(filePath, 'utf-8'));
