03e3596591b849b1693a227bf4d43417e2820134
Atuokwu Fortune <atuokwu.fortune@gmail.com>
2024-01-04 00:07:19 +0000

    task 4

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,1 +1,16 @@
 #!/usr/bin/node
+
+const id = process.argv[2];
+
+const request = require('request');
+
+const url = 'https://swapi-api.alx-tools.com/api/films/' + id;
+
+request.get(url, (err, res, body) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  const bodyObj = JSON.parse(body);
+  console.log(bodyObj.title);
+});
--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,1 +1,25 @@
 #!/usr/bin/node
+
+const url = process.argv[2];
+
+const request = require('request');
+
+const wedge = 'https://swapi-api.alx-tools.com/api/people/18/';
+
+request.get(url, (err, res, body) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  const bodyObj = JSON.parse(body);
+  const results = bodyObj.results;
+  let count = 0;
+  for (let i = 0; i < results.length; i++) {
+    results[i].characters.forEach((item) => {
+      if (wedge === item) {
+        count += 1;
+      }
+    });
+  }
+  console.log(count);
+});
