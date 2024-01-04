cc4cb6369de4046f31bac748d280aa83abe534d1
Atuokwu Fortune <atuokwu.fortune@gmail.com>
2024-01-04 01:01:51 +0000

    task 6

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,1 +1,23 @@
 #!/usr/bin/node
+
+const url = process.argv[2];
+const request = require('request');
+const dict = {};
+request(url, (err, res, body) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  const bodyObj = JSON.parse(body);
+  bodyObj.forEach((item) => {
+    if (item.completed === true) {
+      const id = item.userId;
+      if (id in dict) {
+        dict[id] += 1;
+      } else {
+        dict[id] = 1;
+      }
+    }
+  });
+  console.log(dict);
+});
