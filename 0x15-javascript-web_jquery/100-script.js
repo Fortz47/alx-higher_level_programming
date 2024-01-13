4a94ce5e33d13c27257bce1d0866fc11d878a851
Atuokwu Fortune <atuokwu.fortune@gmail.com>
2024-01-13 17:10:10 +0000

    task 100-101

--- 0x15-javascript-web_jquery/100-script.js
+++ 0x15-javascript-web_jquery/100-script.js
@@ -1,1 +1,5 @@
 #!/usr/bin/node
+
+document.addEventListener('DOMContentLoaded', () => {
+  document.querySelector('header').style.color = '#FF0000';
+});
--- 0x15-javascript-web_jquery/101-script.js
+++ 0x15-javascript-web_jquery/101-script.js
@@ -1,1 +1,20 @@
 #!/usr/bin/node
+
+$(document).ready(() => {
+  const addItem = $('DIV#add_item');
+  const removeItem = $('DIV#remove_item');
+  const clearList = $('DIV#clear_list');
+  const list = $('UL.my_list');
+
+  addItem.on('click', function () {
+    list.append('<li>Item</li>');
+  });
+
+  removeItem.on('click', function () {
+    $('UL.my_list li:last-child').remove();
+  });
+
+  clearList.on('click', function () {
+    list.empty();
+  });
+});
