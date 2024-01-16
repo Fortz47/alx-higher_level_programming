657220ef91943584dde1f786a3a3c65b311d9742
Atuokwu Fortune <atuokwu.fortune@gmail.com>
2024-01-16 10:28:24 +0000

    task 1

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,13 +7,7 @@ import requests
 def number_of_subscribers(subreddit):
     """queries the Reddit API and returns the number of subscribers"""
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': \
-            'Mozilla/5.0 \
-            (Windows NT 10.0; Win64; x64) \
-            AppleWebKit/537.36 \
-            (KHTML, like Gecko) \
-            Chrome/91.0.4472.124 \
-            Safari/537.36'}
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'}
 
     try:
         response = requests.get(url, headers=headers, allow_redirects=False)
